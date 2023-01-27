d504c789f28e1a1bcb7efbde16fdf6831a45763d
Thiago Gonsalves Da Silva <20211011110014@ifrn.local>
2023-01-27 15:59:57 -0300

    Tarefa 2 atuali 1

---
+++ tarefa02/prototipo/POOzap/ListarMembrosWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace POOzap
+{
+    /// <summary>
+    /// Lógica interna para ListarMembrosWindow.xaml
+    /// </summary>
+    public partial class ListarMembrosWindow : Window
+    {
+        public ListarMembrosWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
