91f6c9b00c8b3a64b51f394f64b7605dedde577a
Thiago Gonsalves Da Silva <20211011110014@ifrn.local>
2023-01-30 18:04:32 -0300

    Atuali 03 tarefa 03 e 02

--- tarefa03/prototipo/POOzap/NContato.cs
+++ tarefa03/prototipo/POOzap/NContato.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace POOzap
 {
     internal static class NContato
     {
+        private static List<Contato> contatos = new List<Contato>();
+        public static List<Contato> Listar()
+        {
+            return contatos;
+        }
+
+        public static void Inserir(Contato c)
+        {
+            Abrir();
+            contatos.Add(c);
+        }
+        public static void Salvar()
+        {
+            XmlSerializer x = new XmlSerializer(typeof(List<Contato>));
+            StreamWriter f = new StreamWriter("./brasil", false);
+            x.Serialize(f, contatos);
+            f.Close();
+        }
+        public static void Abrir()
+        {
+            StreamReader f = null;
+            try
+            {
+                XmlSerializer x = new XmlSerializer(typeof(List<Contato>));
+                f = new StreamReader("./brasil", false);
+                contatos = (List<Contato>)x.Deserialize(f);
+            }
+            catch
+            {
+                contatos = new List<Contato>();
+            }
+            if (f != null)f.Close();
+        }
+
     }
 }
--- tarefa03/prototipo/POOzap/NGrupo.cs
+++ tarefa03/prototipo/POOzap/NGrupo.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace POOzap
 {
@@ -11,7 +13,60 @@ namespace POOzap
         private static List<Grupo> grupos = new List<Grupo>();
         public static List<Grupo> Listar()
         {
+            Abrir();
             return grupos;
+        }
+
+        public static void Inserir (Grupo g)
+        {
+            Abrir();
+            grupos.Add(g);
+            Salvar();
+        }
+        public static void Salvar()
+        {
+            XmlSerializer x = new XmlSerializer(typeof(List<Grupo>));
+            StreamWriter f = new StreamWriter("./grupos.xml", false);
+            x.Serialize(f, grupos);
+            f.Close();
+            
+        }
+
+        public static void Abrir()
+        {
+            StreamReader f = null;
+            try
+            {
+                XmlSerializer x = new XmlSerializer(typeof(List<Grupo>));
+                f = new StreamReader("./grupos.xml", false);
+                grupos = (List<Grupo>) x.Deserialize(f);
+            }
+            catch
+            {
+                grupos = new List<Grupo>();
+            }
+            if (f != null) f.Close();
+        }
+
+        public static Grupo Checar(int id) {
+            foreach (Grupo g in grupos)
+                if (g.Id == id) return g;
+            return null;
+        }
+
+        public static void Excluir(Grupo g)
+        {
+            Abrir();
+            grupos.Remove(Checar(g.Id));
+            Salvar();
+        }
+        public static void Atualizar(Grupo g)
+        {
+            Abrir();
+            Grupo n = Checar(g.Id);
+            n.Descricao = g.Descricao;
+            n.Nome = g.Nome;
+            Salvar();
         }
     }
 }
